1b6cc550add9275d6f388fc4b6e22fcb0ed5d5d4
jehanyang <jehanyang@gmail.com>
2018-10-19 08:37:56 -0700

    Added things. Got communication working with Arduino through Serial. Two lines plotting in real time. Will just need a sensor to finish closed loop control.

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
             this.pressureText = new System.Windows.Forms.TextBox();
@@ -46,32 +46,32 @@
             // 
             this.chart1.BackColor = System.Drawing.Color.Transparent;
             this.chart1.BorderlineColor = System.Drawing.Color.Transparent;
-            chartArea2.AxisX.LabelAutoFitStyle = ((System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles)((((System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.DecreaseFont | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.StaggeredLabels) 
+            chartArea1.AxisX.LabelAutoFitStyle = ((System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles)((((System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.DecreaseFont | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.StaggeredLabels) 
             | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.LabelsAngleStep30) 
             | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.WordWrap)));
-            chartArea2.AxisY.LabelAutoFitStyle = ((System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles)((((System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.DecreaseFont | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.StaggeredLabels) 
+            chartArea1.AxisY.LabelAutoFitStyle = ((System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles)((((System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.DecreaseFont | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.StaggeredLabels) 
             | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.LabelsAngleStep30) 
             | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.WordWrap)));
-            chartArea2.BackColor = System.Drawing.Color.Gray;
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.BackColor = System.Drawing.Color.Transparent;
-            legend2.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            legend2.IsTextAutoFit = false;
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea1.BackColor = System.Drawing.Color.Gray;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.BackColor = System.Drawing.Color.Transparent;
+            legend1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            legend1.IsTextAutoFit = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(-25, 33);
             this.chart1.Name = "chart1";
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series3.Legend = "Legend1";
-            series3.Name = "Pressure Sensed";
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series4.Legend = "Legend1";
-            series4.Name = "Input Pressure";
-            this.chart1.Series.Add(series3);
-            this.chart1.Series.Add(series4);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Pressure Sensed";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Input Pressure";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(843, 317);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -104,6 +104,7 @@
             this.pressureBtn.TabIndex = 3;
             this.pressureBtn.Text = "Enter";
             this.pressureBtn.UseVisualStyleBackColor = true;
+            this.pressureBtn.Click += new System.EventHandler(this.pressureBtn_Click);
             // 
             // serialData
             // 
@@ -126,6 +127,7 @@
             this.readSerialButton.TabIndex = 5;
             this.readSerialButton.Text = "Read Serial";
             this.readSerialButton.UseVisualStyleBackColor = true;
+            this.readSerialButton.Click += new System.EventHandler(this.readSerialButton_Click);
             // 
             // label2
             // 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -13,15 +13,17 @@ using System.IO.Ports;
 using System.Threading;
 using System.Runtime.InteropServices.WindowsRuntime;
 using rtChart;
+using System.Diagnostics;
 
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        SerialPort serialPort1 = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+        // Remember to change COM#
+        SerialPort serialPort1 = new SerialPort("COM13", 9600, Parity.None, 8, StopBits.One); 
         string pressInput;
-        kayChart serialDataChart;
+        kayChart serialDataChart, serialDataChart2;
 
         public Form1()
         {
@@ -33,27 +35,69 @@ namespace WindowsFormsApp1
             pressureText.Name = "pressureText";*/
             serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialDataReceivedEventHandler);
             serialPort1.Handshake = Handshake.None;
+            serialPort1.RtsEnable = true;
+            Debug.WriteLine("Send to debug output.");
+            /*serialPort1.Open();
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine();
+            Console.Read();
+            serialPort1.Close();
+            */
+            this.Load += Form1_Load;
+        }
 
+        private void DataReceivedHandler(
+                        object sender,
+                        SerialDataReceivedEventArgs e)
+        {
             
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            /*Console.Write("Data Received: ");
+            Console.WriteLine(indata);*/
+            serialData.Invoke((MethodInvoker)delegate { serialData.AppendText("Data Received: " + indata + "\n"); });
+            /*if (String.Equals(indata, "A"))
+            {
+                serialPort1.DiscardInBuffer();          // clear the serial port buffer
+                serialPort1.Write("A");               // ask for more
+            }*/
         }
 
         private void serialDataReceivedEventHandler(object sender, SerialDataReceivedEventArgs e)
-        {
+        { 
             SerialPort sData = sender as SerialPort;
             string recvData = sData.ReadLine();
-            if (!serialPort1.IsOpen)
+
+            /*byte[] recvData = new byte[2];
+
+            recvData[0] = (byte)sData.ReadByte();
+            recvData[1] = (byte)sData.ReadByte();
+            */
+            /*if (!serialPort1.IsOpen)
             {
                 serialPort1.Open();
+            }*/
+            //serialData.Invoke((MethodInvoker)delegate { serialData.AppendText("Received: " + recvData + "\n"); });
+            if (String.Equals(recvData, "A"))
+            {
+                serialPort1.DiscardInBuffer();          // clear the serial port buffer
+                serialPort1.Write("0.0\n");               // ask for more / handshake
             }
-            serialData.Invoke((MethodInvoker)delegate { serialData.AppendText("Received: " + recvData); });
-            serialPort1.Close();
+            serialData.Invoke((MethodInvoker)delegate { serialData.AppendText("Received: " + recvData + "\n"); });
+            //serialPort1.Close();
 
             // Initialization of Chart Update
-            double data;
+            double data, data2;
             bool result = Double.TryParse(recvData, out data);
+            bool result2 = Double.TryParse(pressInput, out data2);
             if (result)
             {
                 serialDataChart.TriggeredUpdate(data);
+
+            }
+            if (result2)
+            {
+                serialDataChart2.TriggeredUpdate(data2);
             }
             
 
@@ -61,16 +105,20 @@ namespace WindowsFormsApp1
 
         private void pressureBtn_Click(object sender, EventArgs e)
         {
-            pressInput = pressureText.Text;
-            float pressFloat = float.Parse(pressInput);
-            byte[] pressByteArr = BitConverter.GetBytes(pressFloat);
-            if (!serialPort1.IsOpen)
+            pressInput = pressureText.Text + "\n";
+            //Debug.WriteLine(pressInput);
+            //double pressDouble = double.Parse(pressInput);
+            //Debug.WriteLine(pressDouble);
+            //byte[] pressByteArr = BitConverter.GetBytes(pressDouble);
+            //Debug.WriteLine(pressByteArr);
+            if (true)//(!serialPort1.IsOpen)
             {
                 try
                 {
-                    serialPort1.Open();
-                    serialPort1.Write(pressByteArr, 0, 4);
-                    serialPort1.Close();
+                    //serialPort1.Open();
+                    serialPort1.Write(pressInput);
+                    //Debug.WriteLine(BitConverter.ToDouble(pressByteArr, 0));
+                    //serialPort1.Close();
                 }
                 catch
                 {
@@ -83,6 +131,8 @@ namespace WindowsFormsApp1
         {
             serialDataChart = new kayChart(chart1, 60);
             serialDataChart.serieName = "Pressure Sensed";
+            serialDataChart2 = new kayChart(chart1, 60);
+            serialDataChart2.serieName = "Input Pressure";
             //TODO: figure out how to get kayChart to print 2 lines through 2 series
         }
 
@@ -92,9 +142,29 @@ namespace WindowsFormsApp1
             serialData.ScrollToCaret();
         }
 
-        private void Form1_Load_1(object sender, EventArgs e)
+        private void readSerialButton_Click(object sender, EventArgs e)
         {
-
+            byte[] readSerialByteArr = new byte[8];
+            if (!serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Open();
+                    //serialPort1.Read(readSerialByteArr, 0, 8);
+                    //serialPort1.Close();
+                    readSerialButton.Text = "Close";
+                }
+                catch
+                {
+                    MessageBox.Show("Serial Port is busy.");
+                }
+            } else
+            {
+                serialPort1.Close();
+                readSerialButton.Text = "Read Serial";
+            }
+            //serialData.Invoke((MethodInvoker)delegate { serialData.AppendText("Received: " + BitConverter.ToString(readSerialByteArr) + "\n"); });
         }
+
     }
 }
